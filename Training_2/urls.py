3af98478919cb8764ce042e2008cfbe983df593f
Asaf Geva <asaf@purplebit.com>
2013-09-22 17:08:10 +0300

    Django tutorial part 3 (Views and templates)

--- Training_2/urls.py
+++ Training_2/urls.py
@@ -4,12 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Training_2.views.home', name='home'),
-    # url(r'^Training_2/', include('Training_2.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^polls/', include('polls.urls', namespace='polls')),
+                       url(r'^admin/', include(admin.site.urls)),
 )
