e1b481e7b7225a58944ea96b3adb85d9627eb2c9
Asaf Geva <asaf@purplebit.com>
2013-09-23 17:01:27 +0300

    Adding polls, can't see results yet

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponseServerError
-from django.core.urlresolvers import reverse
 from django.views import generic
 
 from polls.models import Poll, Choice
@@ -12,10 +11,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        """Return the last five published polls."""
+        """Return the last ten published polls."""
         return Poll.objects.filter(
             pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        ).order_by('-pub_date')[:10]
 
 
 class DetailView(generic.DetailView):
@@ -28,7 +27,7 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
-def _mark_voted(request,poll_id):
+def _mark_voted(request, poll_id):
     if not 'voted' in request.session:
         request.session['voted'] = []
     # Make sure the user can't vote again on this poll
@@ -56,3 +55,20 @@ def vote(request, poll_id):
             })
         else:
             return HttpResponseRedirect(p.get_absolute_url())
+
+
+def new(request):
+    try:
+        question = request.POST['question']
+        choices = request.POST['choices'].split('\n')
+    except KeyError:
+        return HttpResponseServerError("Missing some values.")
+    else:
+        p = Poll(question=question, pub_date=timezone.now())
+        p.save()
+        for choice in choices:
+            c = Choice(poll=p, choice_text=choice, votes=0)
+            c.save()
+
+        return render(request, 'polls/index.html')
+    pass
