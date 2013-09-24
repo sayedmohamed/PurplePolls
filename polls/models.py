4784ac831a530e3ce9107eb123c9380d1e9e1b66
Asaf Geva <asaf@purplebit.com>
2013-09-24 11:29:35 +0300

    Cleanups

--- polls/models.py
+++ polls/models.py
@@ -17,9 +17,6 @@ class Poll(models.Model):
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
-    def get_absolute_url(self):
-        return u"/polls/%s/" % self.pk
-
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
