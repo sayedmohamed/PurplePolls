af38d119fda003a98cfd1a61003a171e3d1e30f3
asafge <asaf@purplebit.com>
2013-09-25 16:20:37 +0300

    url naming fixes

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,8 @@ from polls import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^new$', views.index_new, name='new'),
-    url(r'^(?P<pk>\d+)/$', views.poll_detail, name='detail'),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^new$', views.index_new, name='index_new'),
+    url(r'^(?P<pk>\d+)/$', views.poll_detail, name='poll_detail'),
+    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='poll_detail_vote'),
 )
 
